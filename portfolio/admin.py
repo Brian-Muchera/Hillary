bc5e8973c89516bef6ed8d5eb09a93f47019ce40
Brian-Muchera <mucherabrian2@gmail.com>
2020-12-11 13:46:13 +0300

    added forms

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile,Blog,Inquiry
+
+admin.site.site_header='Muchera-Brian Admin'
+admin.site.site_title='Muchera-Brian Admin Dashboard'
 
 # Register your models here.
+admin.site.register(Blog)
+admin.site.register(Profile)
+admin.site.register(Inquiry)
